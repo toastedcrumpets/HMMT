fa6fc22eabcad3a40e2ceebedc72b1fd1666cfa0
Marcus Bannerman <m.bannerman@gmail.com>
2024-09-02 22:18:05 +0100

    Started reworking the notes

--- footer.js
+++ footer.js
@@ -14,81 +14,51 @@ loadScripts = function (script_urls, script_callback) {
 
 loadScripts([
     "../reveal.js/dist/reveal.js",
-    "../reveal.js-plugins/audio-slideshow/plugin.js",
-    "../reveal.js-plugins/audio-slideshow/recorder.js",
-    "../reveal.js-plugins/audio-slideshow/RecordRTC.js",
     "../reveal.js/plugin/highlight/highlight.js",
-    "../reveal.js/plugin/search/search.js",
     "../reveal.js/plugin/notes/notes.js",
     "../reveal.js/plugin/math/math.js",
-    "../reveal.js-plugins/menu/menu.js",
+    "../reveal.js-menu/menu.js",
 ],
-    function() {
-	var revealopts = {
-	    //This width and height allows printing to pdf at A4 and is slightly widescreen to give the best all round size
-	    pdfSeparateFragments: false,
-	    width:1920,
-	    height:1080,
-	    margin:0.1,
-	    minScale:0.2,
-	    maxScale:1.5,
-	    slideNumber: 'c / t',
-	    history: true,
-	    transition: 'fade',
-	    backgroundTransition: 'fade',
-	    pdfMaxPagesPerSlide: 1,
-	    controls:true,
-	    plugins: [
-		RevealHighlight,
-		RevealSearch,
-		RevealNotes,
-		RevealMath,
-		RevealMenu,
-		RevealAudioSlideshow,
-		RevealAudioRecorder
-	    ],
-	    menu: {
-		titleSelector:'WILLNOTFINDTITLES',
-		markers: true,
-		hideMissingTitles: true,
-		themes: false,
-		transitions: false,
-		custom: [
-		    { title: 'Lectures', icon: '<i class="fa fa-graduation-cap"></i>', src: 'toc.html' },
-		]
-	    },
-	    chalkboard: { // font-awesome.min.css must be available
-		//src: "chalkboard/chalkboard.json",
-		toggleChalkboardButton: { left: "80px" },
-		toggleNotesButton: { left: "130px" },
-	    },
-	    math : {
-		config: "TeX-AMS_SVG-full",
-		tex2jax: {
-		    inlineMath: [['$','$'],['\\(','\\)']] ,
-		    skipTags: ['script', 'noscript', 'style', 'textarea', 'pre']
-		},
-		TeX: {
-		    packages: {'[-]': ['require', 'autoload']},
-		    extensions: ["cancel.js", "color.js"],
-		    Macros: {
-			bm: ["\\boldsymbol{#1}",1]
-		    },
-		},
-	    }
-	};
-
-	if (!(typeof(audiofiles) === 'undefined')) {
-	    //We have audio files, load the plugins
-	    revealopts.plugins = revealopts.plugins.concat([
-		RevealAudioSlideshow,
-		RevealAudioRecorder
-	    ]);
-	    revealopts.audio = {
-		prefix: audiofiles,
-		autoplay:true,
-	    }
-	}
+	function () {
+		var revealopts = {
+			//This width and height allows printing to pdf at A4 and is slightly widescreen to give the best all round size
+			width: 1920,
+			height: 1080,
+			hash: true,
+			plugins: [
+				RevealHighlight,
+				RevealNotes,
+				RevealMath,
+				RevealMenu
+			],
+			menu: {
+				titleSelector: 'WILLNOTFINDTITLES',
+				markers: false,
+				hideMissingTitles: false,
+				themes: false,
+				transitions: false,
+				custom: [
+					{ title: 'Lectures', icon: '<i class="fa fa-graduation-cap"></i>', src: 'toc.html' },
+				],
+				delayInit: false
+			},
+			math: {
+				config: "TeX-AMS_SVG-full",
+				tex2jax: {
+					inlineMath: [['$', '$'], ['\\(', '\\)']],
+					skipTags: ['script', 'noscript', 'style', 'textarea', 'pre']
+				},
+				TeX: {
+					packages: { '[-]': ['require', 'autoload'] },
+					extensions: ["cancel.js", "color.js"],
+					Macros: {
+						bm: ["\\boldsymbol{#1}", 1]
+					},
+				},
+			},
+			url: 'https://marcusbannerman.co.uk/HMMT',
+			view: 'scroll',
+		};
 
 	Reveal.initialize(revealopts);
 }
--- header.js
+++ header.js
@@ -2,8 +2,7 @@
 document.getElementsByTagName("head")[0].insertAdjacentHTML(
   "beforeend",
   "<link rel=\"stylesheet\" href=\"../reveal.js/dist/reveal.css\" />"
-  +"<link rel=\"stylesheet\" href=\"../reveal.js/dist/theme/beige.css\" />"  
-  +"<link rel=\"stylesheet\" href=\"../MCBreveal.css\" />"  
+  +"<link rel=\"stylesheet\" href=\"../reveal.js/dist/theme/white.css\" />"  
   +"<link rel=\"stylesheet\" href=\"../resources/Font-Awesome/css/font-awesome.min.css\" />"
   +"<link rel=\"shortcut icon\" type=\"image/png\" href=\"favicon.png\">"
 );
