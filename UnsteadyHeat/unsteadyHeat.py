d6fdb833b5a8c184679a482ca8cf79850ad61d88
Marcus Bannerman <m.bannerman@gmail.com>
2024-11-04 23:23:31 +0000

    More work

--- UnsteadyHeat/unsteadyHeat.py
+++
@@ -1,8 +0,0 @@
-from manimlib import *
-
-
-class UnsteadyHeatEquation(InteractiveScene):
-    def construct(self):
-        # Add some simple geometry
-        circle = Circle()
-        self.add(circle)
